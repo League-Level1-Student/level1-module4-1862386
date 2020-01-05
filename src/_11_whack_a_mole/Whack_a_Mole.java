18ad742cda00a0cb8e1d1623fe9e798a1a5bac64
League Student <league@iMac-9.attlocal.net>
2020-01-05 12:57:02 -0800

    Latest Tweet and Whack a Mole

--- src/_11_whack_a_mole/Whack_a_Mole.java
+++ src/_11_whack_a_mole/Whack_a_Mole.java
@@ -1,41 +1,58 @@
 package _11_whack_a_mole;
 
 import java.awt.Container;
+import java.sql.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
 
 public class Whack_a_Mole {
 	public static void main(String[] args) {
 		Whack_a_Mole w = new Whack_a_Mole();
 		w.drawButtons();
 	}
-
+	public static int generateRandomIntIntRange() {
+		Random r = new Random();
+		return r.nextInt((24));
+		
+	}
 	void drawButtons() {
 		JPanel p = new JPanel();
 		JFrame f = new JFrame("Whack a Button for Fame and Glory");
+		int r = generateRandomIntIntRange();
 		for (int i = 0; i < 24; i++) {
-			JButton b = new JButton("mole!");
+			JButton b = new JButton();
 			p.add(b);
+			if (i == r) {
+			b.setText("mole!");
+			}
 		}
 		f.add(p);
 		f.setVisible(true);
-		f.setSize(640, 640);
+		f.setSize(672, 150);
 
 	}
-
+//	if (mousePressed == true) {
+			
+//	}
 	void speak(String words) {
 		try {
-			Runtime.getRuntime().exec("say " + words).waitFor();
+			Runtime.getRuntime().exec("Why would you press it? " + words).waitFor();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static int generateRandomIntIntRange() {
-		Random r = new Random();
-		return r.nextInt((24 - 1) + 1) + 1;
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+	    Date timeAtEnd = new Date(molesWhacked);
+	    JOptionPane.showMessageDialog(null, "Your whack rate is "
+	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	                  + " moles per second.");
 	}
+
 }
